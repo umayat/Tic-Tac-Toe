3054c4129355be5eefeee50b385429cd09e918b2
umayat <umayat@ryerson.ca>
2019-03-06 13:21:51 -0500

    TEST: <Square /> renders player icon

--- src/Components/Square.test.js
+++ src/Components/Square.test.js
@@ -3,12 +3,22 @@ import { shallow } from 'enzyme';
 import Square from './Square';
 
 describe('rendering', () => {
+	let rand = Math.floor((Math.random() * 2));	//random number 0 or 1
+	let playerIcon;
 	let wrapper;
+	
+	//randomly assign user symbol
+	rand === 0 ? playerIcon = 'X' : playerIcon = 'O';
+
 	beforeEach( () => {
-		wrapper = shallow(<Square />)
+		wrapper = shallow(<Square value={playerIcon} />)
 	})
 
 	it('should render a button', () => {
 		expect(wrapper.find('button')).toHaveLength(1)
+	})
+
+	it('should render player icon', () => {
+		expect(wrapper.find('button').contains(playerIcon)).toBe(true)
 	})
 })
