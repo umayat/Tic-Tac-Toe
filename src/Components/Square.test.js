0072a85cabf4b2dfb42d512c1e18da783d6fb493
umayat <umayat@ryerson.ca>
2019-03-06 14:07:58 -0500

    TEST: <Square /> stimulates click

--- src/Components/Square.test.js
+++ src/Components/Square.test.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Square from './Square';
 
-describe('rendering', () => {
+//generate random player icon: 'X' or 'O'
+const getPlayerIcon = () => {
 	let rand = Math.floor((Math.random() * 2));	//random number 0 or 1
 	let playerIcon;
-	let wrapper;
-	
+
 	//randomly assign user symbol
 	rand === 0 ? playerIcon = 'X' : playerIcon = 'O';
 
+	return playerIcon;
+}
+
+describe('rendering', () => {
+	let playerIcon = getPlayerIcon();
+	let wrapper;
+	
 	beforeEach( () => {
 		wrapper = shallow(<Square value={playerIcon} />)
 	})
@@ -20,5 +27,24 @@ describe('rendering', () => {
 
 	it('should render player icon', () => {
 		expect(wrapper.find('button').contains(playerIcon)).toBe(true)
+	})
+})
+
+describe('interaction', () => {
+	let playerIcon = getPlayerIcon();
+	let wrapper;
+	
+	beforeEach( () => {
+		wrapper = shallow(<Square value={playerIcon} onClick={jest.fn()} />)
+	})
+
+	describe('clicking the button', () => {
+		beforeEach( () => {
+			//simulate button click
+			wrapper.find('button').prop('onClick')()
+		})
+		it('should call the onClick callback', () => {
+			expect(wrapper.find('button').prop('onClick')).toHaveBeenCalledTimes(1)
+		})
 	})
 })
