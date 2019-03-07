90a2012ed137f29a5108c8a1386f3f2b2a5fb5bb
umayat <umayat@ryerson.ca>
2019-03-06 23:12:49 -0500

    PASSED: <Board /> renders correctly

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -5,4 +5,31 @@
 import React from 'react';
 import Square from './Square';
 
-export default () => {};
+const Board = (props) => {
+	const rowStart = [1, 4, 7];
+	const colStart = [0, 1, 2];
+
+	const rows = rowStart.map( (col, i) => {
+		const cols = colStart.map( (row, j) => {
+			return(
+				<Square 
+					key={j+i}
+					value={props.value}
+					onClick={props.onClick}
+				/>
+			)
+		})
+
+		return(
+			<div className="board-row" key={i}>{cols}</div>
+		)
+	})
+
+	return(
+		<div>
+			{rows}
+		</div>
+	)
+}
+
+export default Board;
